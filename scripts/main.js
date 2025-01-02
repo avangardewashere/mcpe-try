22f16246c8f416a9c09bc4b6de8852f230d61d0b
avangardewashere <avelpanaligan@gmail.com>
2025-01-02 11:55:14 +0800

    add/plain-world

--- scripts/main.js
+++ scripts/main.js
@@ -16,33 +16,60 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-
-const controls = new OrbitControls(camera,renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 //Scene setup
 
 const scene = new THREE.Scene();
 const geoemetry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00d000 });
+const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 const cube = new THREE.Mesh(geoemetry, material);
 
 scene.add(cube);
+
+//adding the light
+function setUpLights() {
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1, 1, 1);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1, 1, 0.5);
+  scene.add(light2);
+
+  const ambient = new THREE.AmbientLight();
+  ambient.intensity = 0.1;
+  scene.add(ambient);
+}
+
+//creating a world
+
+function setUpWorld(size) {
+  for (let x = 0; x < size; x++) {
+    for (let z = 0; z < size; z++) {
+      const cube = new THREE.Mesh(geoemetry, material);
+      cube.position.set(x, 0, z);
+      scene.add(cube);
+    }
+  }
+}
 
 //render loop
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
-window.addEventListener('resize',()=>{
-  camera.aspect =window.innerWidth/window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+setUpLights();
+setUpWorld(2);
 animate();
-
-
