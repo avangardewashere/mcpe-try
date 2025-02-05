93199e1450fcd33feee7d0011322aa2ea1d5a2c8
avangardewashere <avelpanaligan@gmail.com>
2025-02-06 05:30:21 +0800

    add/constructor

--- repractice/world.js
+++ repractice/world.js
@@ -1,3 +1,9 @@
 import * as THREE from "three";
 const geoemetry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
+export class World extends THREE.Group {
+    constructor(size = { width: 32, height: 16 }) {
+        super();
+        this.size = size;
+      }
+}
