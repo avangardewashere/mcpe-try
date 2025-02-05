5f7edd48ee58a4d84c5d588f153781b730770cc0
avangardewashere <avelpanaligan@gmail.com>
2025-02-06 05:27:02 +0800

    add/resize-event

--- repractice/main.js
+++ repractice/main.js
@@ -43,3 +43,13 @@ function animate() {
 
     renderer.renderer(scene,camera)
 }
+
+
+window.addEventListener("resize",()=>{
+    camera.aspect = window.innerWidth /window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+
+    
+})
+
