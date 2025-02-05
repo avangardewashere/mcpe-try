ea20ff0774027834a611cb530eba51861034cffd
avangardewashere <avelpanaligan@gmail.com>
2025-02-05 21:59:17 +0800

    add/controls

--- repractice/main.js
+++ repractice/main.js
@@ -9,13 +9,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-
 //camera setup
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight
-  );
-  
+  75,
+  window.innerWidth / window.innerHeight
+);
 
-camera.position.set(-32,16,-32);
-camera.lookAt(0,0,0)
+camera.position.set(-32, 16, -32);
+camera.lookAt(0, 0, 0);
+
+//controls
+
+const controls = new OrbitControls(cameraFar, renderer.domElement);
+controls.target.set(16, 0, 16);
+controls.update();
