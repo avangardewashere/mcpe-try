e7fd24f6e2942d65ca075ab10b1f89549c2c5fd3
avangardewashere <avelpanaligan@gmail.com>
2025-02-05 21:57:05 +0800

    add/renderer

--- repractice/main.js
+++ repractice/main.js
@@ -1,3 +1,16 @@
 import * as Three from "three";
-import {OrbitControls} from "three/examples/jsm/Addons.js";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { World } from "../scripts/world";
+
+//render seti
+const renderer = new Three.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+//camera setup
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
