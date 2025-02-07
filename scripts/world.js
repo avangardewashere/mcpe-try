e06cc0d2f0f4e2308b9765fe06880aef0feb7938
avangardewashere <avelpanaligan@gmail.com>
2025-02-08 04:15:14 +0800

    add/ui

--- scripts/world.js
+++ scripts/world.js
@@ -5,7 +5,7 @@ const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 // const cube = new THREE.Mesh(geoemetry, material);
 
 export class World extends THREE.Group {
-  constructor(size = { width: 64, height: 32 }) {
+  constructor(size = { width: 64, height: 2 }) {
     super();
     this.size = size;
   }
@@ -14,17 +14,20 @@ export class World extends THREE.Group {
     const maxCount = this.size.width * this.size.width * this.size.height;
     const mesh = new THREE.InstancedMesh(geoemetry, material, maxCount);
 
-    const matric = new THREE.Matrix4();
+    const matrix = new THREE.Matrix4();
 
     mesh.count = 0;
     for (let x = 0; x < this.size; x++) {
       for (let y = 0; y < this.size.height; y++) {
         for (let z = 0; z < this.size; z++) {
           //     const block = new THREE.Mesh(geoemetry, material);
-          block.position.set(x, y, z);
-          this.add(block);
+          // block.position.set(x, y, z);
+          // this.add(block);
+          matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
+          mesh.setMatrixAt(mesh.count++, matrix);
         }
       }
     }
+    this.add(mesh);
   }
 }
