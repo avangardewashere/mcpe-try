dbdc240bac8baaefa1523c25f7652b8daddeb6c5
avangardewashere <avelpanaligan@gmail.com>
2025-02-08 09:00:06 +0800

    fix/debug-world-generate

--- scripts/ui.js
+++ scripts/ui.js
@@ -3,7 +3,8 @@ import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 export function createUI(world) {
   const gui = new GUI();
 
-  gui.add(world.size, "width",8.,128,1).name('width');
-  gui.add(world.size, "width",8.,64,1).name('height')
+  gui.add(world.size, "width",8,128,1).name('width');
+  gui.add(world.size, "width",4.,64,1).name('height')
   gui.add(world,'generate')
 }
+ 
--- scripts/world.js
+++ scripts/world.js
@@ -5,21 +5,22 @@ const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 // const cube = new THREE.Mesh(geoemetry, material);
 
 export class World extends THREE.Group {
-  constructor(size = { width: 64, height: 2 }) {
+  constructor(size = { width: 32, height: 16 }) {
     super();
     this.size = size;
   }
 
   generate() {
+    this.clear();
     const maxCount = this.size.width * this.size.width * this.size.height;
     const mesh = new THREE.InstancedMesh(geoemetry, material, maxCount);
 
     const matrix = new THREE.Matrix4();
 
     mesh.count = 0;
-    for (let x = 0; x < this.size; x++) {
+    for (let x = 0; x < this.size.width; x++) {
       for (let y = 0; y < this.size.height; y++) {
-        for (let z = 0; z < this.size; z++) {
+        for (let z = 0; z < this.size.width; z++) {
           //     const block = new THREE.Mesh(geoemetry, material);
           // block.position.set(x, y, z);
           // this.add(block);
